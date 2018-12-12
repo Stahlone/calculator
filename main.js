b68419a1eefcbc148704365e2596cd9a8a7540d8
Stahl <fvj.114@hotmail.com>
2018-12-12 19:04:18 +0100

    almost everything working

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+// reset display on page reload
+document.addEventListener("DOMContentLoaded", () => display.value = '');
+
 /**
  * Buttons
  */
 
 // display
 let display = document.querySelector('input');
-// operation and numeric buttons
+// numeric buttons 
+let btnNum = document.querySelectorAll('.btn-num');
+// operator buttons
 let btnOper = document.querySelectorAll('.btn-op');
 // back
 let btnBack = document.getElementById('btn-back');
@@ -18,8 +23,12 @@ let btnResult = document.getElementById('btn-result');
  * Listeners
  */
 
+btnNum.forEach(function(btn){
+    btn.addEventListener('click', addNum);
+})
+
 btnOper.forEach(function(btn){
-    btn.addEventListener('click', addValue);
+    btn.addEventListener('click', addOper);
 })
 
 btnBack.addEventListener('click', back);
@@ -30,26 +39,79 @@ btnResult.addEventListener('click', result);
 
 
 /**
- * Function defintion
+ * Main functions
  */
 
-function addValue(e){
+function addNum(e){
+    removeResultIdAndClear();
     display.value += e.target.value;
 }
 
+function addOper(e){
+    // ability to input negative numbers
+    if (display.value === '' && e.target.value === '-'){
+        display.value = '-';
+    } else if (checkValidDisplay()){
+        display.value += e.target.value;
+        removeResultId();
+    }
+}
+
+// add check for error on eveyr function
 function back(){
-    display.value = display.value.substring(0, display.value.length -1);
+    if (display.value !== 'Error'){
+        display.value = display.value.substring(0, display.value.length -1);
+        removeResultId();
+    }
 }
 
 function clear(){
     display.value = '';
+    removeResultId();
 }
 
 function result(){
-    try {
-        display.value = eval(display.value);
-    } catch(e){
-        display.value = 'Error';
+    if (checkValidDisplay()){
+        if ((/\/0/).test(display.value)){
+            display.value = 'Error';
+        } else {
+            try {
+                display.value = eval(display.value);
+            } catch(e){
+                display.value = 'Error';
+            }
+        }
+        display.setAttribute('id', 'show-result');
+    }
+}
+
+
+/**
+ * Helper and advanced behaviour functions
+ */
+
+// check if display isn't empty, the previous element isn't an operator
+// and if there's an error message
+function checkValidDisplay(){
+    return display.value !== '' && display.value && (/\d$/).test(display.value) && !(/[a-z]/).test(display.value);
+}
+
+// After hitting the result button, pressing any number will clear the display
+// this is handled creating an id of 'show-result' on the display after hitting result
+// this id has to be removed when any other button besides result is pressed
+
+// for operator, clear and back buttons
+function removeResultId(){
+    if (display.getAttribute('id') === 'show-result'){
+        display.removeAttribute('id');
+        return true;
+    }
+}
+
+// for num buttons
+function removeResultIdAndClear(){
+    if (removeResultId()){
+        display.value = '';
     }
 }
 
@@ -59,10 +121,17 @@ function result(){
  */
 
 window.onkeydown = function(e){
+    
     let btnKey = e.key;
+
     if (Object.values(btnOper).some(elem => elem.textContent == btnKey)){
+        removeResultId();
+        display.value += btnKey;
+    } else if (Object.values(btnNum).some(elem => elem.textContent == btnKey)){
+        removeResultIdAndClear();
         display.value += btnKey;
     }
+
     switch(btnKey){
         case 'Backspace':
             back();
@@ -82,57 +151,21 @@ window.onkeydown = function(e){
  * TO-DO
  */
 
-// reset input display upon pressing any numeric button after using the result button
+// limit nunmber input/output to display size
+// only one comma on display FOR EACH NUMBER
+// implement every feature on keyboard
 
-// function result(){
-//     try {
-//         display.value = eval(display.value);
-//     } catch(e){
-//         display.value = 'Error';
-//     }
-//     addClearToBtnOper();
+
+
+// check if there's a mathematical operation to evaluate on display
+// function checkOperation(){
+//     return (/[-+*\/]/).test(display.value);
 // }
 
-// function tempClear(e){
-//     display.value = e.target.value;
-//     btnOper.forEach(function(btn){
-//         btn.removeEventListener('click', tempClear);
-//     })
-//     btnOper.forEach(function(btn){
-//         btn.addEventListener('click', addValue);
-//     })
-// }
+//    /[-+*\/]/  use this regex to split the screen display value
+// and only allow inserting a comma on the last element of the array
+// if it doesn't include one already
 
-// function addClearToBtnOper(){
-//     btnOper.forEach(function(btn){
-//         btn.removeEventListener('click', addValue);
-//     })
-//     btnOper.forEach(function(btn){
-//         btn.addEventListener('click', tempClear);
-//     })
-// }
-
-// this would be a much easier approach
-
-// function result(){
-//     try {
-//         display.value = eval(display.value);
-//     } catch(e){
-//         display.value = 'Error';
-//     }
-//     display.setAttribute('id', 'show-result');
-// }
-
-// function addValue(e){
-//     if (display.getAttribute('id') == 'show-result'){
-//         display.value = '';
-//         display.removeAttribute('id');
-//     }
-//     display.value += e.target.value;
-// }
-
-
-// separate between numbers and operators
-// make the addValue event only work on operators if display is not empty
-// or the previous character is not an operator
-// disable result button if the last character is an operator
+// keyboar support
+// e.key and e.target.value are the key to solve this
+// try to unify this variables depending on the element that triggers the event
